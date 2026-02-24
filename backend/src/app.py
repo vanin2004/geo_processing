ca97761ce3095f4c7f42eb077b7df601cfdccc9c
vanin2004 <160011677+vanin2004@users.noreply.github.com>
2026-02-24 15:49:13 +0300

    Update app.py

--- backend/src/app.py
+++ backend/src/app.py
@@ -3,8 +3,7 @@ from contextlib import asynccontextmanager
 
 from fastapi import FastAPI
 
-# from fastapi.openapi.utils import get_openapi
-from src.config import fastapi_config, settings
+from src.config import app_config
 from src.injectors import initialize_database
 from src.routers.api import router
 from src.routers.handlers import (
@@ -12,8 +11,6 @@ from src.routers.handlers import (
     resource_already_exists_handler,
     resource_not_found_handler,
 )
-
-# from src.services import AlgorithmAbstractFactory
 from src.services import FileAlreadyExistsError, TaskNotFoundError
 from src.services import FileNotFoundError as StorageFileNotFoundError
 
@@ -28,9 +25,9 @@ async def lifespan(app: FastAPI):
 
 
 app = FastAPI(
-    title=settings.app_name,
-    version=settings.app_version,
-    debug=settings.debug,
+    title=app_config.project_name,
+    version=app_config.app_version,
+    debug=app_config.log_level == "debug",
     lifespan=lifespan,
 )
 app.add_exception_handler(TaskNotFoundError, resource_not_found_handler)
@@ -41,17 +38,4 @@ app.add_exception_handler(Exception, global_exception_handler)
 app.include_router(router)
 
 
-if __name__ == "__main__":
-    import uvicorn
-
-    logger.info(
-        f"Starting {settings.app_name} v{settings.app_version} on {fastapi_config.host}:{fastapi_config.port}"
-    )
-
-    uvicorn.run(
-        "src.app:app",
-        host=fastapi_config.host,
-        port=fastapi_config.port if fastapi_config.port else 8000,
-        log_level=fastapi_config.log_level,
-        reload=fastapi_config.reload,
-    )
+logger.info(f"Starting {app_config.project_name} v{app_config.app_version}")
